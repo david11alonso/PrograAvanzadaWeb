89a8f9f567467937cfff50a2ae674bd74037ef69
David_PC\Familia <rarias.david@outlook.com>
2021-04-15 22:47:05 -0600

    ====== David update 15/04/21 22:45============== Se elimino tipo de propuesta en la BD y en el Backend. ==========================================

--- Solution/DataModels/Propuesta.cs
+++ Solution/DataModels/Propuesta.cs
@@ -11,7 +11,6 @@ namespace Solution.API.DataModels
         public string Titulo { get; set; }
         public string Descripcion { get; set; }
         public bool Pendiente { get; set; }
-        public int Tipo { get; set; }
         public string UsuarioId { get; set; }
         public DateTime FechaPublicacion { get; set; }
         public DateTime FechaFinalizacion { get; set; }
