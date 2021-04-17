bbd2dd638bc7dc65e9a9ddfe53e9fd18d36bd716
DESKTOP-O31GV3V\Admin <rarias.david@outlook.com>
2021-04-17 09:03:13 +0000

    ====== David update 17/04/21 3:00============== Se modifico como el sistema revisa el rol para que el usuario no tenga que loguear 2 veces. ==========================================

--- Frontend.API/Controllers/LoginController.cs
+++ Frontend.API/Controllers/LoginController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
 using System.Threading.Tasks;
 using FrontEnd.API.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace FrontEnd.API.Controllers
 {
@@ -10,7 +13,7 @@ namespace FrontEnd.API.Controllers
     {
         private readonly SignInManager<IdentityUser> signInManager;
         private UserManager<IdentityUser> userManager;
-
+        string baseurl = "http://45.79.241.73/";
         public LoginController(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> usrmgr)
         {
             this.signInManager = signInManager;
@@ -29,6 +32,24 @@ namespace FrontEnd.API.Controllers
         {
             return View();
         }
+        private List<AspNetUserRoles> GetRoleByUserId(string id)
+        {
+            List<AspNetUserRoles> aux = new List<AspNetUserRoles>();
+            using (var cl = new HttpClient())
+            {
+                cl.BaseAddress = new Uri(baseurl);
+                cl.DefaultRequestHeaders.Clear();
+                cl.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage res = cl.GetAsync("api/AspNetUserRoles/" + id).Result;
+
+                if (res.IsSuccessStatusCode)
+                {
+                    var auxres = res.Content.ReadAsStringAsync().Result;
+                    aux = JsonConvert.DeserializeObject<List<AspNetUserRoles>>(auxres);
+                }
+            }
+            return aux;
+        }
 
         [HttpPost]
         public async Task<IActionResult> Index(LoginViewModel model)
@@ -37,21 +58,27 @@ namespace FrontEnd.API.Controllers
             {
                 var result = await signInManager.PasswordSignInAsync(
                     model.Email, model.Password, model.RememberMe, false);
-
                 if (result.Succeeded)
                 {
-                    if (User.IsInRole("Admin"))
+                    IdentityUser user = await userManager.FindByEmailAsync(model.Email);
+                    var currentRole = GetRoleByUserId(user.Id);
+                    foreach(AspNetUserRoles role in currentRole)
                     {
-                        return RedirectToAction("index", "home");
+                        if (role.Role.Name =="Admin")
+                        {
+                            return RedirectToAction("index", "home");
 
-                    }else if (User.IsInRole("Empleado"))
-                    {
-                        return RedirectToAction("index", "homeEmpleado");
+                        }
+                        else if (role.Role.Name == "Empleado")
+                        {
+                            return RedirectToAction("index", "homeEmpleado");
 
+                        }
                     }
+                    
                 }
 
-                ModelState.AddModelError(string.Empty, "Invalid Login Attempt");
+                ModelState.AddModelError(string.Empty, "Error de autenticacion. Por favor volver a intentarlo.");
             }
 
             return View(model);
