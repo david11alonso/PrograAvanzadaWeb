98eae18f279215ab91a055f39089a1f7fe3f7123
DESKTOP-O31GV3V\Admin <rarias.david@outlook.com>
2021-04-18 01:45:26 +0000

    ====== David update 17/04/21 20:00============== Reportes FrontEnd ==========================================

---
+++ FrontEnd.API/FrontEnd/API/Models/ReporteVotos.cs
@@ -0,0 +1,10 @@
+﻿namespace FrontEnd.API.Models
+{
+    public class ReporteVotos
+    {
+        public virtual Propuesta Propuesta { get; set; }
+        public int DeAcuerdo { get; set; }
+        public int DeSacuerdo { get; set; }
+        public int Neutral { get; set; }
+    }
+}
---
+++ FrontEnd.API/Models/ReporteVotos.cs
@@ -0,0 +1,16 @@
+﻿using FrontEnd.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Frontend.API.Models
+{
+    public class ReporteVotos
+    {
+        public virtual Propuesta Propuesta { get; set; }
+        public int DeAcuerdo { get; set; }
+        public int DeSacuerdo { get; set; }
+        public int Neutral { get; set; }
+    }
+}
